bd427e590d838ba858ee6e4258564cdfc2eda446
Oleh Kozlovskyi <okozlovskyi@n-ix.com>
2020-02-13 14:08:09 +0200

    connect api to apollo server

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,19 +1,35 @@
 import { ApolloServer, gql } from 'apollo-server-micro';
-import connectDB from '../../lib/mongoose';
+import { mergeResolvers, mergeTypeDefs } from 'graphql-toolkit';
 
-const typeDefs = gql`
+import connectDB from '../../lib/mongoose';
+import { habitsResolvers } from '../../api/habits/resolvers';
+import { habitsMutations } from '../../api/habits/mutations';
+import Habits from '../../api/habits/Habits.graphql';
+
+const fakeTypeDefs = gql`
   type Query {
     sayHello: String
   }
 `;
 
-const resolvers = {
+const fakeResolvers = {
   Query: {
     sayHello: () => {
       return 'Hello Level Up';
     }
   }
 };
+
+const resolvers = mergeResolvers([
+  fakeResolvers,
+  habitsResolvers,
+  habitsMutations
+]);
+
+const typeDefs = mergeTypeDefs([
+  fakeTypeDefs,
+  Habits
+]);
 
 const apolloServer = new ApolloServer({ typeDefs, resolvers });
 
