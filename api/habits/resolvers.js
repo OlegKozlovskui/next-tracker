f2a98376b0bab15ead3c7fdb9df302ef01fbc53c
Oleh Kozlovskyi <kozlovskuioleg@gmail.com>
2020-02-17 18:07:19 +0200

    finish events

--- api/habits/resolvers.js
+++ api/habits/resolvers.js
@@ -1,3 +1,6 @@
+import { GraphQLScalarType } from 'graphql';
+import { Kind } from 'graphql/language';
+
 import Habits from './habits';
 
 export const habitsResolvers = {
@@ -10,5 +13,21 @@ export const habitsResolvers = {
         console.log(e);
       }
     }
-  }
+  },
+  Date: new GraphQLScalarType({
+    name: 'Date',
+    description: 'Date type',
+    parseValue(value) {
+      return new Date(value);
+    },
+    serialize(value) {
+      return value.getTime();
+    },
+    parseLiteral(ast) {
+      if (ast.kind === Kind.INT) {
+        return new Date(ast.value);
+      }
+      return null;
+    }
+  })
 };
